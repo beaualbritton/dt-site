76803456a1de343e358739e16a482714eeb12b57
caveblind <albrittoncb@gmail.com>
2025-07-14 01:06:11 -0400

    quick tailwind check

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,14 @@
-<script>
-  import './app.css';
+<script lang="ts">
+  import "./app.css";
 </script>
 
 <main>
-<p>first commit!</p>
+  <h1
+    class="flex justify-start items-center ml-15 mt-30 mb-5 font-semibold text-4xl"
+  >
+    direc-tuit
+  </h1>
+  <p class="ml-15 italic text-xs">
+    a simple way to visualize files in your terminal
+  </p>
 </main>
-
-

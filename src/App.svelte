27a78efd6af7be0387c421128329ec3917e59a05
caveblind <albrittoncb@gmail.com>
2025-07-15 19:34:37 -0400

    feat: bouncing splash text
    using tailwind's built in bounce, and delaying it by a ms gap in between each character

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,26 @@
-<script lang="ts">
+<script>
   import "./app.css";
+  var bouncingText = "public beta!";
+  console.log("public beta!".split(""));
+  function bounceChar(char) {
+    if (char === " ") return "\u00A0\u00A0";
+    return char;
+  }
 </script>
 
 <main>
-  <h1
-    class="flex justify-start items-center ml-15 mt-30 mb-5 font-semibold text-4xl"
-  >
-    direc-tuit
-  </h1>
-  <p class="ml-15 italic text-xs">
+  <div class="flex justify-start items-center">
+    <h1 class="ml-15 mt-30 mb-5 font-semibold text-4xl">direc-tuit</h1>
+    <div class="flex flex-row ml-2.5 mt-20 italic text-xs text-yellow-200">
+      {#each bouncingText.split("") as character, i}
+        <p class="m-0.125 animate-bounce" style="animation-delay: {i * 75}ms">
+          {bounceChar(character)}
+        </p>
+      {/each}
+    </div>
+  </div>
+
+  <p class="ml-15 italic text-sm">
     a simple way to visualize files in your terminal
   </p>
 </main>

b438278c8e1e9d21e2c5dc9808eef94288664d52
caveblind <albrittoncb@gmail.com>
2025-07-18 13:53:34 -0400

    feat: transition animation for sections

--- source/App.svelte
+++ source/App.svelte
@@ -4,29 +4,40 @@
   import About from "./sections/About.svelte";
   $:sectionArray= [Header,About];
   $:currentSectionIndex = 0;
-  function navigateSectionDown()
+  $:animationFlag = false;
+  $:animationDuration = 500;//ms
+  async function navigateSectionDown()
   {
+    animationFlag = true;
+
+    await new Promise(p => setTimeout(p, animationDuration));
     if(currentSectionIndex < sectionArray.length)
     {
       ++currentSectionIndex;
     }
+    animationFlag = false;
   }
-  function navigateSectionUp()
+  async function navigateSectionUp()
   {
+    animationFlag = true;
+    await new Promise(p => setTimeout(p, animationDuration));
     if(currentSectionIndex > 0)
     {
       --currentSectionIndex;
     }
+    animationFlag = false;
   }
   $: currentSection = sectionArray[currentSectionIndex];
 </script>
 
 <main class="flex items-center justify-center h-screen flex-col">
-  <div class="inline-block">
+  <div class={`inline-block transition-all ease-in-out duration-${animationDuration} ${animationFlag? 'opacity-0 translate-y-4' : 'opacity-100 translate-y-0'}`}>
     {#if currentSectionIndex > 0}
     <Separator on:click={navigateSectionUp} middleChar="▲"/>
     {/if}
-    <svelte:component this = {currentSection}/>
+    <div >
+      <svelte:component  this = {currentSection}/>
+    </div>
     {#if currentSectionIndex < sectionArray.length}
     <Separator on:click={navigateSectionDown} middleChar="▼" />
     {/if}

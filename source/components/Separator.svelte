ffedf96f79b019dc40a119f97412a3f160be6746
caveblind <albrittoncb@gmail.com>
2025-07-19 00:20:28 -0400

    fix: styling separator so animations sync, circumventing mount delay
    tailwindcss built in animations weren't working so i scoured online for some basic pulse/bounce animation. maybe add separate bounce for up/down separators

--- source/components/Separator.svelte
+++ source/components/Separator.svelte
@@ -5,6 +5,6 @@
   $: renderString = "═".repeat(sepWidth / 5);
 </script>
 
-<button on:click bind:clientWidth={wWidth} class="flex items-center justify-center w-full w-screen-max h-0.5 mt-10 text-white/60 animate-pulse rounded-full" style="animation-duration:2s">
-  {renderString} <div class="text-3xl"> {middleChar} </div> {renderString}
+<button on:click bind:clientWidth={wWidth} class="flex items-center justify-center w-full w-screen-max mt-10 text-white/60 sync-pulse rounded-full" style="animation-duration:2s">
+  {renderString} <div class="text-3xl text-white/90 sync-bounce" style="animation-duration: 3s;"> {"\u00A0"+middleChar+"\u00A0"} </div> {renderString}
 </button>

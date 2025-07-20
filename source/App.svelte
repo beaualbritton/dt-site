5f43d2605695f712f488409cb98df717ae5056c0
caveblind <albrittoncb@gmail.com>
2025-07-20 19:06:42 -0400

    small feat: navbar structure & uiw

--- source/App.svelte
+++ source/App.svelte
@@ -2,7 +2,8 @@
   import Separator from "./components/Separator.svelte";
   import Header from "./sections/Header.svelte";
   import About from "./sections/About.svelte";
-  $:sectionArray= [Header,About];
+  import Nav from "./sections/Nav.svelte"
+  let sectionArray= [Header,About];
   $:currentSectionIndex = 0;
   $:animationFlag = false;
   $:animationDuration = 500;//ms
@@ -30,16 +31,19 @@
   $: currentSection = sectionArray[currentSectionIndex];
 </script>
 
-<main class="flex items-center justify-center h-screen flex-col">
-  <div class={`inline-block transition-all ease-in-out duration-${animationDuration} ${animationFlag? 'opacity-0 translate-y-4' : 'opacity-100 translate-y-0'}`}>
-    {#if currentSectionIndex > 0}
-    <Separator on:click={navigateSectionUp} middleChar="▲"/>
-    {/if}
-    <div >
-      <svelte:component  this = {currentSection}/>
+<main class ="flex items-center flex-col h-screen">
+  <Nav />
+  <div class = "flex items-center justify-center h-screen flex-col">
+    <div class={`inline-block transition-all ease-in-out duration-${animationDuration} ${animationFlag? 'opacity-0 translate-y-4' : 'opacity-100 translate-y-0'}`}>
+      {#if currentSectionIndex > 0}
+      <Separator on:click={navigateSectionUp} middleChar="▲"/>
+      {/if}
+      <div >
+        <svelte:component  this = {currentSection}/>
+      </div>
+      {#if currentSectionIndex < sectionArray.length}
+      <Separator on:click={navigateSectionDown} middleChar="▼" />
+      {/if}
     </div>
-    {#if currentSectionIndex < sectionArray.length}
-    <Separator on:click={navigateSectionDown} middleChar="▼" />
-    {/if}
   </div>
 </main>
---
+++ source/sections/Nav.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+ let navList : String[] = ["Demo","Download", "About",];
+
+  
+</script>
+
+
+<div class = "mt-10 flex flex-items justify-center">
+  {#each navList as buttonName, i}
+  <button class = "px-8 pt-1 bg-black/10 rounded-lg text-lg font-bold">{buttonName}</button>
+  {/each}
+</div>
